1ac765ad6b7531f3fb9d585e6701908b67b3ae89
krugis <158180572+krugis@users.noreply.github.com>
2025-07-18 17:43:42 +0300

    Update config.py

--- services/pulse-content-api/app/core/config.py
+++ services/pulse-content-api/app/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    KAFKA_BOOTSTRAP_SERVERS: str = "kafka:2902"
+    KAFKA_BOOTSTRAP_SERVERS: str = "kafka:29092"
     KAFKA_POSTS_TOPIC: str = "new_posts"
     OPENSEARCH_HOST: str = "opensearch"
     OPENSEARCH_PORT: int = 9200
