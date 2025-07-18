5b12e5265c68433a7e04075629f354acf6fa83fd
krugis <158180572+krugis@users.noreply.github.com>
2025-07-18 16:17:24 +0300

    Update config.py

--- services/pulse-ingest/app/core/config.py
+++ services/pulse-ingest/app/core/config.py
@@ -5,7 +5,9 @@ class Settings(BaseSettings):
     # This will automatically load the APIFY_API_TOKEN from an environment variable.
     # The model_config line tells pydantic to look for a .env file if it exists.
     APIFY_API_TOKEN: str
-
+    KAFKA_BOOTSTRAP_SERVERS: str = "kafka:29092"
+    KAFKA_POSTS_TOPIC: str = "new_posts"
+    
     model_config = SettingsConfigDict(env_file=".env")
 
 # Create a single instance to be used across the application
